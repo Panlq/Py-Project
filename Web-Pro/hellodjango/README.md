280dc87b8a1d92f5e9803b8e660a2265c378bee0
Panlq <18960311163@163.com>
2020-08-10 01:46:44 +0800

    1. 增加markdown 语法和代码块高亮的模块

--- Web-Pro/hellodjango/README.md
+++ Web-Pro/hellodjango/README.md
@@ -1,2 +1,7 @@
 博客数据库表设计
 
+## django web 结合markdown 语法+显示代码块
+- python markdown   markdown基本语法模块
+- Pygments 是通用语法高亮显示器，可以帮助自动生成美化代码块的样式文件 
+
+[Django搭建个人博客：使用Markdown语法书写文章](https://segmentfault.com/a/1190000016459780)
--- Web-Pro/hellodjango/blog/views.py
+++ Web-Pro/hellodjango/blog/views.py
@@ -1,3 +1,4 @@
+import markdown
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from .models import Post
@@ -11,4 +12,10 @@ def index(request):
 
 def detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
+    post.body = markdown.markdown(post.body,
+                                  extensions=[
+                                      'markdown.extensions.extra',
+                                      'markdown.extensions.codehilite',  # 语法高亮
+                                      'markdown.extensions.toc',   # 允许自动生成目录
+                                  ])
     return render(request, 'blog/detail.html', context={'post': post})
